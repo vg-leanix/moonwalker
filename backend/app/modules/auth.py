1925d958c4a2059537f1cd24da8f58a59d124b49
Keerthan22 <keerthan.avula@leanix.net>
2021-07-30 16:21:01 +0530

    Updated changes to put provisioning

--- backend/app/modules/auth.py
+++ backend/app/modules/auth.py
@@ -1,3 +1,4 @@
+from app.utils.put_provisioning import ProvisionReqBody
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -5,6 +6,7 @@ import os
 from typing import List, Optional
 import app.utils.lx as lx
 import app.utils.put_processor as int_api
+import app.utils.put_provisioning as provisioning
 
 K8S_PROCESSOR = "app/modules/k8s.json"
 router = APIRouter(
@@ -16,9 +18,13 @@ router = APIRouter(
 class Config(BaseModel):
     apiToken: str
     edition: str
-    instance: str
+    host: str
     wsName: str
 
+class ProvisioningBody(BaseModel):
+    config: Config
+    provisionReqBody: ProvisionReqBody
+    apply_base_model: bool
 
 @router.post("/createws", tags=["auth"])
 async def create_workspace(config: Config):
@@ -36,6 +42,26 @@ async def create_workspace(config: Config):
         raise HTTPException(
             status_code=400,
             detail="Error in Workspace setup workflow",
+
+        )
+
+    return JSONResponse(output)
+
+@router.post("/updateProvisioning", tags=["auth"])
+async def update_provisioning(param: ProvisioningBody):
+    bearer_token = lx.authenticate(host=param.config.host, apitoken=param.config.apiToken)
+
+    status_code, error = provisioning.put_provisioning(host=param.config.host, jwt_token=bearer_token,apply_base_model=True, extenstions=param.provisionReqBody.extensions)
+
+    if not error and ((status_code == 200 ) or (status_code == 204)):
+        output = {
+            "message": "workspace successfully provisioned",
+            "status_code_int_api": status_code
+        }
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="Error in Workspace provision workflow",
 
         )
 
---
+++ backend/app/utils/put_provisioning.py
@@ -0,0 +1,42 @@
+import json
+from typing import List
+import requests
+from requests import status_codes
+from pydantic import BaseModel
+
+class ProvisionReqBody(BaseModel):
+    extensions: List[str]
+    api_token: str
+
+def put_provisioning(host: str, jwt_token: str, apply_base_model: bool, extenstions: List[str]):
+    pass
+    request_url = f'https://{host}.leanix.net/services/provisioning/v1/provision'
+    auth_header = 'Bearer ' + jwt_token
+    header = {'Authorization': auth_header, "Content-Type": "application/json"}
+    reqBody: ProvisionReqBody = {
+        extenstions: extenstions
+    }
+
+    parameters = {
+        'apply_base_model': apply_base_model,
+        'skip_charts': False,
+        'skip_dashboards': False,
+        'skip_meta_model': False,
+        'skip_portals': False,
+        'skip_saved_reports': False,
+        'skip_saved_searches': False,
+        'skip_tag_groups': False,
+        'skip_settings': False,
+        'skip_violations': False
+    }
+
+    try:
+        r = requests.put(request_url, body=reqBody, params=parameters, headers=header)
+        status_code = r.status_code
+        error = None
+
+    except Exception as e:
+        error = str(e)
+        status_code = None
+
+    return status_code, error
